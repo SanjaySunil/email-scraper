bd3d42706ab35c9a454bae51aa352d1109445c07
Sanjay Sunil <sanjaysunil@protonmail.com>
2022-10-06 19:51:38 +0100

    feat(readme): add auto-generated readme

---
+++ .github/make_readme.mjs
@@ -0,0 +1,38 @@
+import README from "readme-components";
+let template = new README({});
+
+template.metadata = {
+  name: "email-scraper",
+  description: "Generate thousands of temporary emails within seconds!",
+  report_bug:
+    "https://github.com/SanjaySunil/email-scraper/issues/new?assignees=&labels=Bug&template=bug_report.md&title=%5BBUG%5D",
+  request_feature:
+    "https://github.com/SanjaySunil/email-scraper/issues/new?assignees=&labels=Suggestions&template=suggestions.md&title=%5BSUGGESTION%5D",
+  author: {
+    name: "Sanjay Sunil",
+    email: "sanjaysunil@protonmail.com",
+  },
+  license: "MIT",
+};
+
+template.use_premade("header", {
+  name: template.metadata.name,
+  description: template.metadata.description,
+  report_bug: template.metadata.report_bug,
+  request_feature: template.metadata.request_feature,
+});
+
+template.use_premade("description", {
+  name: template.metadata.name,
+  long_description:
+    "is a powerful temporary email generator that allows you to scrape thousands of emails Python! Email Scraper uses [fyii.de trashmail provider](https://fyii.de/trashmail/) to generate emails. All emails are unique and have their own inboxes in which emails can be received from. Use email-scraper to use services where you don't want to provide your own email address.",
+});
+
+template.use_component("./readme_components/install.md");
+template.use_premade("license", {
+  author: template.metadata.author.name,
+  email: template.metadata.author.email,
+  license: template.metadata.license,
+});
+
+template.make_readme();
---
+++ .github/readme_components/install.md
@@ -0,0 +1,16 @@
+## Prerequisites
+
+Ensure you have **Python 3** installed. `pip` will also be required to install important packages for the tool.
+
+### Requirements
+
+UUID is the only requirement in this project.
+
+```sh
+pip install uuid
+```
+
+### Usage
+
+Run `main.py` to start email-scraper menu.
+
