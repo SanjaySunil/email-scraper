a1d76326e900f7a01d59e9ec0f32211ae8b8564e
Sanjay Sunil <sanjaysunil@protonmail.com>
2022-11-15 21:27:23 +0000

    fix(email_scraper): use double underscore convention for private attributes

--- src/email_scraper.py
+++ src/email_scraper.py
@@ -11,28 +11,28 @@ import webbrowser
 
 class EmailScraper:
     def __init__(self, file_path):
-        self._DOMAIN = "fyii.de/trashmail/"
-        self._EMAIL_DOMAIN = "@fyii.de"
-        self._file_path = file_path
+        self.__DOMAIN = "fyii.de/trashmail/"
+        self.__EMAIL__DOMAIN = "@fyii.de"
+        self.__file_path = file_path
 
     def __scrape(self):
         random_uuid = str(uuid.uuid4())
-        email_link = self._DOMAIN+random_uuid[:8]+'.html'
-        email = random_uuid[:8]+self._EMAIL_DOMAIN
+        email_link = self.__DOMAIN+random_uuid[:8]+'.html'
+        email = random_uuid[:8]+self.__EMAIL__DOMAIN
         return email, email_link
 
     def start(self, emails):
         scraped_emails = 0
-        exists = os.path.isfile(self._file_path)
+        exists = os.path.isfile(self.__file_path)
 
         try:
           if exists:
-              file_handler = open(self._file_path, "a")
+              file_handler = open(self.__file_path, "a")
           else:
-              file_handler = open(self._file_path, "w+")
+              file_handler = open(self.__file_path, "w+")
 
         except FileNotFoundError:
-          if self._file_path == '': return 'Cannot write to a file with no name!'
+          if self.__file_path == '': return 'Cannot write to a file with no name!'
           else: return 'Cannot write to specified file!'
 
         while scraped_emails != emails:
@@ -42,4 +42,4 @@ class EmailScraper:
 
         file_handler.close()
 
-        return f'Added {emails} emails to {self._file_path}!'
+        return f'Added {emails} emails to {self.__file_path}!'
