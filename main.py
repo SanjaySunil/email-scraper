62c4bc9973c9a602214770ff1fc5d6789cf67628
Sanjay Sunil <sanjaysunil@protonmail.com>
2022-11-15 21:17:21 +0000

    feat: added ability to write emails to a file of user's choice

--- main.py
+++ main.py
@@ -3,8 +3,48 @@
  * Author: Sanjay Sunil
  * License: MIT
 """
-
+import os
+import webbrowser
 from src.email_scraper import EmailScraper
 
-scraper = EmailScraper(file_path='emails.txt')
-scraper.menu()
+OPTIONS = ["Scrape 1 Email", "Scrape Multiple Emails",
+           "GitHub", "Exit"]
+os.system('clear')
+
+while True:
+    os.system('clear')
+    print("Email Scraper\n")
+    [print(i+1, OPTIONS[i]) for i in range(len(OPTIONS))]
+    print('\n')
+
+    while True:
+      try:
+        option = int(input("Choose an option: "))
+        break
+      except ValueError:
+        print('Please type an option number.\n')
+
+    if option == 1:
+        file_to_write = input('Enter filename to write to: ')
+        scraper = EmailScraper(file_path=file_to_write)
+        scraper.start(1)
+        back_to_menu = input(
+            f'Added 1 email to {scraper._file_path}!\n\nPress enter to go back to menu. ')
+
+    elif option == 2:
+        emails = int(input("Emails to scrape: "))
+        file_to_write = input('Enter filename to write to: ')
+        scraper = EmailScraper(file_path=file_to_write)
+        scraper.start(emails)
+        back_to_menu = input(
+            f'Added {emails} emails to {scraper._file_path}!\n\nPress enter to go back to menu. ')
+
+    elif option == 3:
+        webbrowser.open(scraper._GITHUB_URL)
+
+    elif option == 4:
+      exit("Thank you for using email-scraper.")
+
+    else:
+        try_again = input(
+            'Invalid option, please enter to try again. ')
--- src/email_scraper.py
+++ src/email_scraper.py
@@ -16,16 +16,13 @@ class EmailScraper:
         self._GITHUB_URL = "https://github.com/sanjaysunil/email-scraper"
         self._file_path = file_path
 
-    def __clear_screen(self):
-        os.system('clear')
-
     def __scrape(self):
         random_uuid = str(uuid.uuid4())
         email_link = self._DOMAIN+random_uuid[:8]+'.html'
         email = random_uuid[:8]+self._EMAIL_DOMAIN
         return email, email_link
 
-    def __start(self, emails):
+    def start(self, emails):
         scraped_emails = 0
         exists = os.path.isfile(self._file_path)
 
@@ -40,32 +37,3 @@ class EmailScraper:
             scraped_emails += 1
 
         file_handler.close()
-
-    def menu(self):
-        OPTIONS = [["Scrape 1 Email"], ["Scrape Multiple Emails"], [
-            "GitHub"], ["Exit"]]
-        self.__clear_screen()
-
-        while True:
-            self.__clear_screen()
-            print("Email Scraper\n")
-            for count, option in enumerate(OPTIONS):
-                print(f"{count+1}: {option[0]}")
-            option = int(input("Choose an option: "))
-
-            if option == 1:
-                self.__start(1)
-                back_to_menu = input(
-                    f'Added 1 email to {self._file_path}!\n\nPress enter to go back to menu. ')
-            elif option == 2:
-                emails = int(input("Emails to scrape: "))
-                self.__start(emails)
-                back_to_menu = input(
-                    f'Added {emails} emails to {self._file_path}!\n\nPress enter to go back to menu. ')
-
-            elif option == 3:
-                webbrowser.open(self._GITHUB_URL)
-
-            else:
-                try_again = input(
-                    'Invalid option, please enter to try again. ')
